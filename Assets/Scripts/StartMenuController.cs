b600356c71e24359cbef8803634fd8534e9840f6
Ekamjot Singh <ejdhindsa@gmail.com>
2026-02-20 15:12:30 -0600

    Create a basic main menu

---
+++ Assets/Scripts/StartMenuController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenuController : MonoBehaviour
+{
+    public void onStartButtonClicked()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }  // end of onStartButtonClicked
+
+    public void onExitButtonClicked()
+    {
+        #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+    }  // end of onExitButtonClicked
+}
